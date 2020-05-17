b69652ebb57aaa4f6d9412e32064969f3d2a23e6
John Pulford <johnpulford@gmail.com>
2020-05-17 11:55:08 +0100

    Fully working example that will reread a file on any updates to the file.

--- Example.py
+++ Example.py
@@ -1,38 +1,44 @@
-import requests
 import rx
-import json
 import os
 from rx import operators as ops
-def filternames(x):
-   if (x["name"].startswith("C")):
-      return x["name"]
-   else :
-      return ""
-content = requests.get('https://jsonplaceholder.typicode.com/users')
-y = json.loads(content.text)
-source = rx.from_(y)
-case1 = source.pipe(
-   ops.filter(lambda c: filternames(c)),
-   ops.map(lambda a:a["name"])
-)
-case1.subscribe(
-   on_next = lambda i: print("Got - {0}".format(i)),
-   on_error = lambda e: print("Error : {0}".format(e)),
-   on_completed = lambda: print("Job Done!"),
-)
+
+"""
+This code will check modified date of a file every second and, 
+if the modified date has changed since last read
+reread the file contents.
+"""
 
 
 myinput_path = "/dev/shm/myinput.txt"
 
 
-def getmodifieddate(file_path):
+def get_modified_date(file_path):
    return os.path.getmtime(file_path)
 
-print(getmodifieddate(myinput_path))
+def read_file_contents(file_path):
+   with open(file_path, 'r') as reader:
+      # Read and print the entire file line by line
+      for line in reader:
+         print(line, end='')
+
 
 file_check_interval_observable = rx.interval(1)
 
-file_check_interval_observable.subscribe_(print)
+file_check_interval_observable.subscribe(print)
 
-file_changed_observable = file_check_interval_observable.subscribe_(lambda i: getmodifieddate(myinput_path))
+file_changed_observable = file_check_interval_observable.pipe(
+   ops.map(lambda i: get_modified_date(myinput_path)),
+   ops.distinct_until_changed()
+)
+
+file_changed_observable.subscribe(
+   on_next = lambda i: print("Got date modified - {0}".format(i)),
+   on_error = lambda e: print("Error : {0}".format(e)),
+   on_completed = lambda: print("Job Done!"),
+)
+
+file_changed_observable.subscribe(print)
+
+file_changed_observable.subscribe(lambda ignore: read_file_contents(myinput_path))
+
 input("press any key to exit")
