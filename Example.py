41b5daee604cd35aee55316ecfb1fe0baa643bba
John Pulford <johnpulford@gmail.com>
2020-05-17 12:00:12 +0100

    Add comments

--- Example.py
+++ Example.py
@@ -22,23 +22,27 @@ def read_file_contents(file_path):
          print(line, end='')
 
 
+# Every second
 file_check_interval_observable = rx.interval(1)
 
+# Print elapsed second
 file_check_interval_observable.subscribe(print)
 
+#emit modified date every second, if the date has changed since last time.
 file_changed_observable = file_check_interval_observable.pipe(
    ops.map(lambda i: get_modified_date(myinput_path)),
    ops.distinct_until_changed()
 )
 
+#Debug subscription to show when modified date gets printed out.
 file_changed_observable.subscribe(
    on_next = lambda i: print("Got date modified - {0}".format(i)),
    on_error = lambda e: print("Error : {0}".format(e)),
    on_completed = lambda: print("Job Done!"),
 )
 
-file_changed_observable.subscribe(print)
-
+#When a emission is produced upstream, read contents of the new file.
 file_changed_observable.subscribe(lambda ignore: read_file_contents(myinput_path))
 
+#Hang main execution so that rx async runs.
 input("press any key to exit")
